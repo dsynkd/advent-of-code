15de118d7206fb28b322c7081ae7712e7a99a18f
Darren Sadr <darrensadr@gmail.com>
2025-12-21 13:57:18 -0800

    .

--- 2025/3/3.lobby.1.py
+++ 2025/3/3.lobby.1.py
@@ -2,9 +2,10 @@ res = 0
 
 for line in open(0):
     bank = list(map(int, line.strip()))
-    # Find max joltage
-    m1 = max(bank[:-1])
-    m2 = max(bank[bank.index(m1) + 1:])
-    res += int(f'{m1}{m2}')
+
+    digit1 = max(bank[:-1])
+    digit2 = max(bank[bank.index(digit1) + 1:])
+
+    res += int(f'{digit1}{digit2}')
 
 print(res)
--- 2025/3/3.lobby.2.py
+++ 2025/3/3.lobby.2.py
@@ -1,14 +1,16 @@
 res = 0
+n = 12
 
 for line in open(0):
     bank = list(map(int, line.strip()))
-    # Find max joltage
-    jolt = ''
-    for i in range(11):
-        digit = max(bank[:i - 11])
+    joltage = ''
+
+    for i in range(1,n):
+        offset = i - n
+        digit = max(bank[:offset])
         bank = bank[bank.index(digit) + 1:]
-        jolt += str(digit)
-    print('jolt:', jolt)
-    res += int(jolt)
+        joltage += str(digit)
+    
+    res += int(joltage)
 
 print(res)
